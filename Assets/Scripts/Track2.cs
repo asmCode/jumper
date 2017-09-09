0858085a23c2d081a6c599f92ea22c79dfc45ade
Szymon <majewskis@gmail.com>
2017-09-08 22:31:05 -0700

    auto connecting platforms

--- Assets/Scripts/Track2.cs
+++ Assets/Scripts/Track2.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Track2 : MonoBehaviour
 {
-    public PlatformJumpPointView platform1;
-    public PlatformJumpPointView platform2;
+    private void OnEnable()
+    {
+        EditorApplication.hierarchyWindowChanged += OnHierarchyChanged;
+    }
+
+    private void OnDisable()
+    {
+        EditorApplication.hierarchyWindowChanged -= OnHierarchyChanged;
+    }
+
+    private void OnHierarchyChanged()
+    {
+        UpdatePlatformConnections();
+    }
 
     private void Update()
     {
-        float trajectoryDistance;
-        float closestDistance = JumpResolver.GetClosestDistance(
-            platform1.Position,
-            platform1.GetJumpSpeed(),
-            platform1.GetJumpAngle(),
-            platform2.Position,
-            out trajectoryDistance);
+    }
 
-        // Debug.LogFormat("closestDistance = {0:0.00}, trajectoryDistance = {1:0.00}", closestDistance, trajectoryDistance);
+    private void UpdatePlatformConnections()
+    {
+        if (transform.childCount == 0)
+            return;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var childPrev = i > 0 ? transform.GetChild(i - 1).GetComponent<PlatformJumpPointView>() : null;
+            var child = transform.GetChild(i).GetComponent<PlatformJumpPointView>();
+            var childNext = i < transform.childCount - 1 ? transform.GetChild(i + 1).GetComponent<PlatformJumpPointView>() : null;
+
+            child.m_prevPlatform = childPrev;
+            child.m_nextPlatform = childNext;
+        }
     }
 }
