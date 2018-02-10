54d4bee1935cb0bd296e13d59234779d83bb86c6
Maciej Pietruszajtys <32144219+BoosterGuy@users.noreply.github.com>
2018-02-10 23:40:58 +0100

    Dodanie loadera.

--- Assets/Scripts/WelcomeScene/WelcomeScene.cs
+++ Assets/Scripts/WelcomeScene/WelcomeScene.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class WelcomeScene : MonoBehaviour
 {
     public string m_nextScene = "BoostersEpicLevel";
 
+	public GameObject loadingScreen;
+	public Slider slider;
+
+	AsyncOperation async;
+
     public void UiEvent_StartPressed()
     {
-        SceneManager.LoadScene(m_nextScene);
+		StartCoroutine(LoadGame());
     }
+
+	IEnumerator LoadGame()
+	{
+		loadingScreen.SetActive (true);
+		async = SceneManager.LoadSceneAsync(m_nextScene);
+		async.allowSceneActivation = false;
+
+		while (async.isDone == false)
+		{
+			slider.value = async.progress;
+			if (async.progress == 0.9f)
+			{
+				slider.value = 1.0f;
+				async.allowSceneActivation = true;
+			}
+			yield return null;
+		}
+	}
 }
